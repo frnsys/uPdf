59ca37e15e49294069a5c6d31a0ed305bc815c7c
Francis Tseng <f+accounts@frnsys.com>
2018-04-18 19:38:49 -0400

    include package data

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from setuptools import setup
 setup(
     name='updf',
     version='1.0.0',
+    description='Tool for manipulating PDFs',
     author='Lorenzo Carbonell Cerezo',
     author_email='lorenzo.carbonell.cerezo@gmail.com',
     license='GPLv3',
+    zip_safe=True,
+    package_data={'': ['data']},
+    include_package_data=True,
     packages=['updf'],
     install_requires=[
     ],
